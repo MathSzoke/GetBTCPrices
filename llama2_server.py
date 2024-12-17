235d316d9aee33cba7cd825e603af175c1f11f1c
Matheus Szoke <matheusszoke@gmail.com>
2024-12-17 10:16:00 -0300

    [feat] Added new starter to llama2 server

--- llama2_server.py
+++ llama2_server.py
@@ -25,3 +25,9 @@ def generate_text(request: ChatRequest):
         return {"response": result[0]["generated_text"]}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
